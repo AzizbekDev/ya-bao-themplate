47c1c656133c1b9d23e4d53fcf1dbc5dfccfd513
AzizbekDev <azizbek_muzaffarov@mail.ru>
2023-11-14 18:07:34 +0500

    home page half finished

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,1 +1,15 @@
-console.log('Connected..');
+document.addEventListener('DOMContentLoaded', function () {
+  var splide = new Splide('#slider', {
+    type: 'loop',
+    perPage: 2,
+    focus: 'center',
+    gap: 20,
+    with: '100vh',
+    fixedWidth: 1100,
+    arrows: false,
+    pagination: false,
+    autoplay: true,
+    interval: 5000,
+  });
+  splide.mount();
+});
