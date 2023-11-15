660d052f2cfb4973092cd4790b639e185852893c
AzizbekDev <azizbek_muzaffarov@mail.ru>
2023-11-15 16:26:47 +0500

    contact page finished

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var splide = new Splide('#slider', {
-    type: 'loop',
-    perPage: 2,
-    focus: 'center',
-    gap: 20,
-    with: '100vh',
-    fixedWidth: 1100,
-    arrows: false,
-    pagination: false,
-    autoplay: true,
-    interval: 5000,
-  });
-  splide.mount();
+  if (document.getElementById('slider')) {
+    var splide = new Splide('#slider', {
+      type: 'loop',
+      perPage: 2,
+      focus: 'center',
+      gap: 20,
+      with: '100vh',
+      fixedWidth: 1100,
+      arrows: false,
+      pagination: false,
+      autoplay: true,
+      interval: 5000,
+    });
+    splide.mount();
+  }
 });
